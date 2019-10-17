8fdb96288fc0e8e89b0e415fd355a5030fda868b
Melissa Knapp <melknapp@gmail.com>
2019-10-16 22:21:30 -0400

    Created basic html, sent variables from the js file to the html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,12 +19,50 @@ var HalloweenWords = ["skeleton",
             "Dracula"
 ]
 
+//Randomly choose one word from the array HalloweenWords for the user to guess
 var wordToGuess =  HalloweenWords[Math.floor(Math.random() * HalloweenWords.length)]; 
+
+//Initialize counter of how many wins at 0
+var winCounter = 0;
+
+//Initialize counter of how many guesses left at 12
+var guessesLeft = 12; 
 
 console.log("The word to guess is " + wordToGuess);
 
+//initializes lettersGuessed to the html file id "letters-guessed"
+//and sets it as an empty array
+
+var lettersGuessed = document.getElementById("letters-guessed"); 
+
+var alreadyGuessed= [];
+
+console.log(lettersGuessed);
+
+ lettersGuessed.textContent = "Hello";
+
+//grabs "wins-amount" from the html 
+var scoreCounter = document.getElementById("wins-amount");
+
+//initializes counter as equal to zero, and sends it back
+//to "wins-amount" in the html file
+var counter = 0;
+scoreCounter.textContent = counter;
+console.log(scoreCounter);
+
+
+
 document.onkeyup = function(event){
     var userInput = event.key;
+    alert("You just pushed " + userInput);
+
+    lettersGuessed.push(userInput);
+    console.log(lettersGuessed);
+
+
+    lettersGuessed.textContent = "userInput is " + userInput;
+
+
 
 }
     console.log("The javascript is running");
